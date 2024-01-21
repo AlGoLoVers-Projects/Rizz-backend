673a734a2dfef892f7588fbaa9686f253b33681b
sooryasrajan <soorya.s27@gmail.com>
2024-01-22 00:25:53 +0530

    Added gitignore

--- routes/authentication.js
+++ routes/authentication.js
@@ -1,9 +1,9 @@
-var express = require('express');
-var router = express.Router();
+const express = require('express');
+const router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
+router.get('/login', function(req, res, next) {
+  //TODO: Get data from env and authenticate
 });
 
 module.exports = router;
